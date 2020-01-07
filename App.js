e8c182ec8cf1fa2bd0f944d70838498aea2cfa2a
Andres Cifuentes <aacifuentes@gmail.com>
2020-01-07 09:24:09 -0600

    created custom hook outside of tutorial for count

--- App.js
+++ App.js
@@ -12,19 +12,54 @@ const Box = ({ backgroundColor = '#3cae6f', scale = 1}) => (
   ]} />
 )
 
-const usePulse = () => {
+const usePulse = (startDelay = 500) => {
 
-    const scale = new Animated.Value(1)
+    const scale = useRef(new Animated.Value(1)).current;
+
+    const pulse = () => {
+      Animated.sequence([
+        Animated.timing(scale, { toValue: 1.2 }),
+        Animated.timing(scale, { toValue: 0.8 }),
+      ]).start(() => pulse())
+    }
+
+    useEffect(() =>{
+      let interval = setTimeout(() => pulse(), startDelay)
+
+      return () => clearInterval(interval)
+    }, [])
+
+    
 
     return scale;
 }
 
-const App = ({ count }) => {
+const useCount = () => {
+  const [count, setCount] = useState(1)
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setCount(count + 1)
+    }, 500)
+
+    return () => clearInterval(interval)
+  })
+
+  return count;
+}
+
+const App = () => {
+
+  
 
   const scale = usePulse()
+  const scale2 = usePulse(750)
+  const count = useCount()
+
 
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'space-around'}}>
+      <Box scale={scale2} backgroundColor={'blue'} />
       <Text>{count}</Text>
       <Box scale={scale} />
     </View>
